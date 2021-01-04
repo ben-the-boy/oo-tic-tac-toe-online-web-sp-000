8b5dd308de0449353270d60a7fc6075faabf81b1
Ben Hayes <b.t.hayes@comcast.net>
2021-01-04 19:42:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,14 +51,7 @@ class TicTacToe
     def turn
       puts "Please enter 1-9."
       input = gets.strip
-      index = input_to_index(input)
-      if valid_move?(index)
-        move(index, current_player)
-        display_board
-      else
-        puts "Invalid"
-        turn
-      end
+
     end
       
         
