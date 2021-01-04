b324c722337535200ad852f4e34f90affa77d743
Ben Hayes <b.t.hayes@comcast.net>
2021-01-04 19:34:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,6 @@ class TicTacToe
       input = gets.strip
       index = input_to_index(input)
       if valid_move?(index)
-        player = current_player
         move(index, player)
         display_board
       else
